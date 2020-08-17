436ff43a1d1503d6405a22af228be0b05ac1cdf6
muldoons2 <59346412+muldoons2@users.noreply.github.com>
2020-08-16 21:09:40 -0400

    Update AyudaMutuaSMS.py

--- AyudaMutuaSMS.py
+++ AyudaMutuaSMS.py
@@ -10,12 +10,11 @@ def home(): return render_template('home.html')
 @app.route("/sample_form")
 def sample_form(): return render_template('sample_form.html')
 
-@app.route('/submit',methods = ['GET','POST'])
+@app.route('/submit', methods = ['POST'])
 def submit():
-    if request.method == 'POST':
-        recipient = request.form['recipient']
-        return render_template('submit.html',recipient=recipient)
-    return render_template('submit.html')
+    recipient = request.form['recipient']
+    return render_template('submit.html',recipient=recipient)
+
 
 
 if __name__ == '__main__':
