88306429f484abb79d73302ae4d8ec5bc7dd7ac8
muldoons2 <59346412+muldoons2@users.noreply.github.com>
2020-08-16 20:54:05 -0400

    Update AyudaMutuaSMS.py

--- AyudaMutuaSMS.py
+++ AyudaMutuaSMS.py
@@ -7,31 +7,12 @@ app = Flask(__name__)
 @app.route("/")
 def home(): return render_template('home.html')
 
-# @app.route("/submit")
-# def submit():
-#     ye_quote= requests.get("https://api.kanye.rest").json()['quote']
-#     return render_template('kanye.html',value=ye_quote)
-
-############### #Twilio API
-
-# # Download the helper library from https://www.twilio.com/docs/python/install
-# from twilio.rest import Client
-
-
-# # Your Account Sid and Auth Token from twilio.com/console
-# # DANGER! This is insecure. See http://twil.io/secure
-# account_sid = 'AC0f09286ecbe74943e0513a4fa26a8310'
-# auth_token = '8fb298894527a2c7673fdab7e39754ec'
-# client = Client(account_sid, auth_token)
-
-# message = client.messages \
-#                 .create(
-#                      body="Test Text. Built by Sean Muldoon on the Twilio API",
-#                      from_='+19175254826',
-#                      to='+19144699732'
-#                  )
-
-# print(message.sid)
+@app.route('/submit',methods = ['GET','POST'])
+def submit():
+    if request.method == 'POST':
+        recipient = request.form['recipient']
+        return render_template('submit.html',recipient=recipient)
+    return render_template('submit.html')
 
 
 if __name__ == '__main__':
