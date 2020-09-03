872a1408ee58a5dc59527aff8775d6bcacf999f0
muldoons2 <59346412+muldoons2@users.noreply.github.com>
2020-09-03 16:57:56 -0400

    Update AyudaMutuaSMS.py

--- AyudaMutuaSMS.py
+++ AyudaMutuaSMS.py
@@ -6,20 +6,17 @@ app = Flask(__name__)
 app.config.update(dict(SECRET_KEY="powerful secretkey", WTF_CSRF_SECRET_KEY="a csrf secret key"))
 
 ###################################################
-def send_sms(from_num,to_num,sid,token,body_text):
-    account_sid = sid
-    auth_token = token
-    client = Client(account_sid, auth_token)
-    message = client.messages \
-                    .create(
-                         body=body_text,
-                         from_=from_num,
-                         to= to_num
-                     )
-# for y in nums:
-#     send_sms(SBM_num)
-
 twil_list=[]
+def send_sms(x,from_num,sid,token,body_text):
+        account_sid = sid
+        auth_token = token
+        client = Client(account_sid, auth_token)
+        message = client.messages \
+                        .create(
+                             body=body_text,
+                             from_=from_num,
+                             to= x
+                         )
 
 ###################################################
 @app.route('/')
@@ -30,16 +27,20 @@ def index():
 @app.route("/register", methods=["POST"])
 def register():
     from_num = request.form.get("from_num")
-    to_num = request.form.get("to_num")
+    num1 = request.form.get("num1")
+    num2 = request.form.get("num2")
+    num3 = request.form.get("num3")
     sid = request.form.get("sid")
     token = request.form.get("token")
     body_text = request.form.get("body_text")
     twil_list.append(from_num)
-    twil_list.append(to_num)
     twil_list.append(sid)
     twil_list.append(token)
     twil_list.append(body_text)
-    send_sms(from_num,to_num,sid,token,body_text)
+    nums=[num1,num2,num3]
+    twil_list.append(nums)
+    for x in nums:
+        send_sms(x,from_num,sid,token,body_text)
     return redirect('/twilio')
 
 ###################################################
